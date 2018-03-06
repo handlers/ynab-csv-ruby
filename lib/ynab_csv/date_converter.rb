c810deaa24229381f74ce2d8e9409b5bfb2678ef
Tim Kein <tim@kein.fail>
2018-03-06 21:21:33 +0100

    Support DMY date format

--- lib/ynab_csv/date_converter.rb
+++ lib/ynab_csv/date_converter.rb
@@ -13,6 +13,8 @@ module YnabCsv
     def to_date
       if date_string.match(/\//)
         Date.strptime(date_string, "%m/%d/%Y")
+      elsif date_string.match(/\d\d\.\d\d\.\d\d\d\d/)
+        Date.strptime(date_string, "%d.%m.%Y")
       else
         Date.strptime(date_string, "%b %Y")
       end
